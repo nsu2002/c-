862b4bc18b1b22c622ed987d17c7b70b39d9e5b9
nsu_2002 <nguyensu59469@gmail.com>
2021-10-05 21:10:30 +0700

    Initial Commit

---
+++ task6/Task06-u40/Task06-u40/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06_u40.Models
+{
+    class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string CoverImage { get; set; }
+
+    }
+    
+}
---
+++ task6/Task06-u40/Task06-u40/Models/BookManager .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06_u40.Models
+{
+    class BookManager
+    {
+        public static List<Book> GetBooks()
+        {
+            var books = new List<Book>();
+
+            books.Add(new Book { BookId = 1, Title = "Vulpate", Author = "Futurum", CoverImage = "Asset/1.png" });
+            books.Add(new Book { BookId = 2, Title = "Mazim", Author = "Sequiter Que", CoverImage = "Asset/2.png" });
+            books.Add(new Book { BookId = 3, Title = "Elit", Author = "Tempor", CoverImage = "Asset/3.png" });
+        }
+    }
+}
+
+
