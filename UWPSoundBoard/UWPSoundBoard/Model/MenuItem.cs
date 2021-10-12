b37597fc8c8b108f819e74722e2968a8b184a3f0
nsu_2002 <nguyensu59469@gmail.com>
2021-10-12 21:19:23 +0700

    task 8

---
+++ UWPSoundBoard/UWPSoundBoard/Model/MenuItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPSoundBoard.Model
+{
+    class MenuItem
+    {
+        public string iconFile { get; set; }
+        public SoundCategory Category { get; set; }
+    }
+}
