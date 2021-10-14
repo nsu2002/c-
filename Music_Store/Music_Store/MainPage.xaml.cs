2985f55e1fbf10539cfd84e3b311640345c548f4
nsu_2002 <nguyensu59469@gmail.com>
2021-10-14 16:30:38 +0700

    music store

---
+++ Music_Store/Music_Store/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Music_Store.models;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Music_Store
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<sound> Sounds;
+        private bool istart = false;
+        private bool isRepeat = false;
+        private string soundSouce = "";
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Sounds = new ObservableCollection<sound>();
+            soundmanager.GetAllSounds(Sounds);
+        }
+
+        private void Search_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+
+        }
+
+        private void SoundGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var sound = (sound)e.ClickedItem;
+            Nhaccuatui.Source = new Uri(this.BaseUri, sound.audioFile);
+            ResultName.Text = sound.Name;
+            soundSouce = sound.audioFile;
+            istart = true;
+            imgSound.Content = "||";
+        }
+
+        private void MyProgressbar_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            if (soundSouce == "")
+            {
+                return;
+            }
+           
+        }
+
+        private void Nhaccuatui_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            imgSound.Content = "||";
+        }
+
+        private void imgSound_Click(object sender, RoutedEventArgs e)
+        {
+            if (soundSouce == "")
+            {
+                return;
+            }
+            if (!istart)
+            {
+                imgSound.Content = "||";
+                istart = true;
+                Nhaccuatui.Play();
+            }
+            else
+            {
+                imgSound.Content = "►";
+                istart = false;
+                Nhaccuatui.Pause();
+            }
+        }
+
+        private void Nhaccuatui_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            imgSound.Content = "►";
+            if (isRepeat)
+            {
+                Nhaccuatui.Play();
+            }
+        }
+
+    }
+}
---
+++ Music_Store/Music_Store/models/soundmanager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music_Store.models
+{
+    class soundmanager
+    {
+        public static void GetAllSounds(ObservableCollection<sound> sounds)
+        {
+            var allSounds = GetSounds();
+            sounds.Clear();
+            allSounds.ForEach(a => sounds.Add(a));
+
+        }
+        public static void GetAllSoundsByName(ObservableCollection<sound> sounds, string name)
+        {
+            var allSounds = GetSounds();
+            var filterSound = allSounds.Where(p => p.Name == name).ToList();
+            sounds.Clear();
+            filterSound.ForEach(a => sounds.Add(a));
+
+        }
+        public static List<sound> GetSounds()
+        {
+            var sounds = new List<sound>();
+            sounds.Add(new sound("Anh Thề đấy", "y2mate.com - Anh Thề Đấy  Thanh HưngCukak Remix Audio Lyrics Video.mp3", "zingmp3.png"));
+            sounds.Add(new sound("Cưới Đi", "y2mate.com - 2T  CƯỚI ĐI Changg Ft Yang Remix  Bản Remix Vinahouse Phiêu Không Lối Thoát  OFFICIAL AUDIO.mp3", "zingmp3.png"));
+            sounds.Add(new sound("Có Hẹn Với Thanh Xuân", "y2mate.com - Có Hẹn Với Thanh Xuân  MonstarCukak Remix Audio Lyrics Video.mp3 (1).webm", "zingmp3.png"));
+            sounds.Add(new sound("Thế Lương", "y2mate.com - Thê Lương  Phúc Chinh  KProx Remix   Nhạc Trẻ EDM Tik Tok Gây Nghiện Hay Nhất.mp3", "zingmp3.png"));
+            sounds.Add(new sound("Hạ Còn Vương Nắng", "y2mate.com - HẠ CÒN VƯƠNG NẮNG  DATKAA x KIDO x Prod QT BEATZ OFFICIAL MUSIC VIDEO.mp3", "zingmp3.png"));
+            sounds.Add(new sound("Chiều Hôm ấy", "y2mate.com - JayKii  CHIỀU HÔM ẤY Official MV.mp3", "zingmp3.png"));
+            sounds.Add(new sound("Kẹo Bông Gòn", "y2mate.com - Kẹo Bông Gòn  H2K ft TrunkyCukak Remix Audio Lyrics Video.mp3", "zingmp3.png"));
+            sounds.Add(new sound("Nụ Cười 18 20", "y2mate.com - Nụ Cười 18 20  Doãn Hiếu Official  Hãy hết mình trên con đường ta chọn hẹn gặp khi thành công.mp3", "zingmp3.png"));
+
+            sounds.Add(new sound("Forget Me Nơ", "y2mate.com - Forget Me Now  Fishy ft Trí DũngCukak Remix Audio Lyrics.mp3", "zingmp3.png"));
+            sounds.Add(new sound("Nụ Cười 18 20", "y2mate.com - Nụ Cười 18 20  Doãn Hiếu Official  Hãy hết mình trên con đường ta chọn hẹn gặp khi thành công.mp3", "zingmp3.png"));
+
+
+            return sounds;
+        }
+    }
+}
