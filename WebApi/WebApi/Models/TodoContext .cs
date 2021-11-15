1fe67652d3ef3684c5a4f0b406521505dd7dccb5
nsu_2002 <nguyensu59469@gmail.com>
2021-11-15 10:47:17 +0700

    api

---
+++ WebApi/WebApi/Models/TodoContext .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace WebApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
